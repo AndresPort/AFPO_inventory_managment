96dc81e57897a26094868cddf146ba427b33045d
Andrés Felipe Portillo O. <andresfelipe29portillo@gmail.com>
2025-06-23 21:41:49 -0500

    Se agregaron optionals para la busqueda en BillRepository

--- AFPOInventaryManagement/src/main/java/AFPOInventaryManagement/Repositories/BillRepository.java
+++ AFPOInventaryManagement/src/main/java/AFPOInventaryManagement/Repositories/BillRepository.java
@@ -4,6 +4,12 @@ import AFPOInventaryManagement.Models.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BillRepository extends JpaRepository<Bill,Long> {
+
+    Optional<Bill> findBillByIdClient(Long idClient);
+    Optional<Bill> findBillByIdPaymentMethod(Long idPaymentMethod);
+    Optional<Bill> findBillByIdSalePoint(Long idSalePoint);
 }
